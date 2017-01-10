f81e21c9c6170bb8f7737388e3da9ac91e1cf010
James Carlson <jxxcarlson@gmail.com>
2017-01-09 22:27:46 -0500

    Add mnemonix to project

--- mix.exs
+++ mix.exs
@@ -18,7 +18,7 @@ defmodule LookupPhoenix.Mixfile do
   # Type `mix help compile.app` for more information.
   def application do
     [mod: {LookupPhoenix, []},
-     applications: [:phoenix, :phoenix_pubsub, :phoenix_html, :cowboy, :logger, :gettext,
+     applications: [:mnemonix, :phoenix, :phoenix_pubsub, :phoenix_html, :cowboy, :logger, :gettext,
                     :phoenix_ecto, :postgrex]]
   end
 
@@ -38,7 +38,8 @@ defmodule LookupPhoenix.Mixfile do
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:gettext, "~> 0.11"},
      {:cowboy, "~> 1.0"},
-     {:comeonin, "~> 2.0"}]
+     {:comeonin, "~> 2.0"},
+     {:mnemonix, "~> 0.2.0"}]
   end
 
   # Aliases are shortcuts or tasks specific to the current project.
