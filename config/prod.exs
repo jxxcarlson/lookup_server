93457340affc05d110332e8c15da256bd1ec1f2f
James Carlson <jxxcarlson@gmail.com>
2017-01-17 15:33:28 -0500

    correct db config for heroku

--- config/prod.exs
+++ config/prod.exs
@@ -19,7 +19,7 @@ config :lookup_phoenix, LookupPhoenix.Endpoint,
   cache_static_manifest: "priv/static/manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
-config :hello_phoenix, LookupPhoenix.Repo,
+config :lookup_phoenix, LookupPhoenix.Repo,
   adapter: Ecto.Adapters.Postgres,
   url: System.get_env("DATABASE_URL"),
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
