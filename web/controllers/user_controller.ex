6c547c2bfaa688acd05f69285f78af28edc7569e
James Carlson <jxxcarlson@gmail.com>
2017-01-16 22:32:58 -0500

    User login implmented

--- web/controllers/user_controller.ex
+++ web/controllers/user_controller.ex
@@ -1,12 +1,12 @@
 defmodule LookupPhoenix.UserController do
   use LookupPhoenix.Web, :controller
+  plug :authenticate when action in [:index, :show]
 
   alias LookupPhoenix.User
 
   def index(conn, _params) do
-    users = Repo.all(LookupPhoenix.User)
-    render conn, "index.html", users: users
-
+     users = Repo.all(LookupPhoenix.User)
+     render conn, "index.html", users: users
   end
 
   def new(conn, _params) do
@@ -20,10 +20,22 @@ defmodule LookupPhoenix.UserController do
     case Repo.insert(changeset) do
       {:ok, user} ->
         conn
+        |> LookupPhoenix.Auth.login(user)
         |> put_flash(:info, "#{user.name}, you are now a Lookup user")
         |> redirect(to: user_path(conn, :index))
       {:error, changeset} ->
         render(conn, "new.html", changeset: changeset)
+    end
+  end
+
+  defp authenticate(conn, _opts) do
+    if conn.assigns.current_user do
+      conn
+    else
+      conn
+      |> put_flash(:error, "You must be signed in to access that page")
+      |> redirect(to: page_path(conn, :index))
+      |> halt
     end
   end
 
--- web/router.ex
+++ web/router.ex
@@ -19,6 +19,7 @@ defmodule LookupPhoenix.Router do
 
     resources "/notes", NoteController
     resources "/users", UserController, only: [:index, :show, :new, :create]
+    resources "/sessions", SessionController, only: [:new, :create, :delete]
 
     get "/random", SearchController, :random
 
