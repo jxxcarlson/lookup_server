e6172873f0365e8bd28c71dcdabd3414aebf3b7e
James Carlson <jxxcarlson@gmail.com>
2017-01-18 14:12:35 -0500

    Better smartLinks

--- web/models/note.ex
+++ web/models/note.ex
@@ -148,7 +148,7 @@ defmodule LookupPhoenix.Note do
     end
 
     def makeSmartLinks(text) do
-          Regex.replace(~r/\s((http|https):\/\/[a-zA-Z0-9\.\-\/&=\?#!@_%]*)\/(\S*\S)\s/, " "<>text<>" ",  " <a href=\"\\1\" target=\"_blank\">\\3</a> ")
+          Regex.replace(~r/\s((http|https):\/\/([a-zA-Z0-9\.\-\/&=\?#!@_%]*))\/(\S*\S)\s/, " "<>text<>" ",  " <a href=\"\\1\" target=\"_blank\">\\1</a> ")
     end
 
 
