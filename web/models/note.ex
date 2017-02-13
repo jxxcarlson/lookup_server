ce58fabe00aa8d22122c85312d453f17ab456b5c
James Carlson <jxxcarlson@gmail.com>
2017-02-13 12:19:35 -0700

    Add channel to Note.decode_query_string

--- web/models/note.ex
+++ web/models/note.ex
@@ -310,12 +310,14 @@ defmodule LookupPhoenix.Note do
 
       # Example: q_string=index=4&id_list=35%2C511%2C142%2C525%2C522%2C531%2C233
       query_data = q_string|> Utility.parse_query_string
-      index = query_data["index"]
-      {index, _} = Integer.parse index
 
+      # Get inputs
+      index = query_data["index"]; {index, _} = Integer.parse index
       id_string = query_data["id_string"] |> String.replace("%2C", ",")
-      id_list = String.split(id_string, ",") # |> Enum.map(fn(id) -> String.trim(id) end)
+      id_list = String.split(id_string, ",")
+      channel = query_data["channel"]
 
+     # Compute outputs
       current_id = Enum.at(id_list, index)
       note_count = length(id_list)
       last_index = note_count - 1
@@ -331,21 +333,22 @@ defmodule LookupPhoenix.Note do
         previous_index = index - 1
       end
 
-      Utility.report("In Note, decode_query_string", q_string)
-
-
       last_id = Enum.at(id_list, last_index)
       next_id = Enum.at(id_list, next_index)
       previous_id = Enum.at(id_list, previous_index)
       first_id = Enum.at(id_list, 0)
 
-      %{first_index: 0, index: index, last_index: last_index,
+      # Assemble output
+      output = %{first_index: 0, index: index, last_index: last_index,
         previous_index: previous_index, next_index: next_index,
         first_id: first_id, last_id: last_id,
         previous_id: previous_id, current_id: current_id, next_id: next_id,
         id_string: id_string, id_list: id_list,
-        note_count: note_count}
+        note_count: note_count, channel: channel}
 
+      Utility.report("Note, decode_query_string", output)
+
+      output
    end
 
    ## ONE-TIME ##
