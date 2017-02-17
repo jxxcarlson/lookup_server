b8670be1252fdcc4375177d50fa9770a0b4a7855
James Carlson <jxxcarlson@gmail.com>
2017-02-17 10:11:44 -0700

    Add mailto feature

--- web/controllers/note_controller.ex
+++ web/controllers/note_controller.ex
@@ -121,9 +121,10 @@ defmodule LookupPhoenix.NoteController do
     inserted_at= Note.inserted_at_short(note)
     updated_at= Note.updated_at_short(note)
     word_count = RenderText.word_count(note.content)
+    email_body = "This note courtesy of http://www.lookupnote.io\n\n" <> note.content |> String.replace("\n", "%0D%0A    ")
 
     params1 = %{note: note, inserted_at: inserted_at, updated_at: updated_at,
-                  options: options, word_count: word_count}
+                  options: options, word_count: word_count, email_body: email_body}
     params2 = Note.decode_query_string(conn.query_string)
     params = Map.merge(params1, params2)
 
