8d3a326c0e95578cae9883277c8ebaf392a0df1b
James Carlson <jxxcarlson@gmail.com>
2017-02-19 10:34:44 -0500

    Add field "shared" (boolean) to notes

---
+++ priv/repo/migrations/20170219153000_add_shared_to_notes.exs
@@ -0,0 +1,9 @@
+defmodule LookupPhoenix.Repo.Migrations.AddSharedToNotes do
+  use Ecto.Migration
+
+  def change do
+    alter table(:notes) do
+      add :shared, :boolean, default: false
+    end
+  end
+end
