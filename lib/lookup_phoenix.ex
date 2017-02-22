13d8ac69f9cfd4be6d87e28fba18242953714528
James Carlson <jxxcarlson@gmail.com>
2017-02-22 16:34:05 -0500

    Add periodic task to refresh tags

--- lib/lookup_phoenix.ex
+++ lib/lookup_phoenix.ex
@@ -15,6 +15,7 @@ defmodule LookupPhoenix do
       supervisor(LookupPhoenix.Endpoint, []),
       # Start your own worker by calling: LookupPhoenix.Worker.start_link(arg1, arg2, arg3)
       # worker(LookupPhoenix.Worker, [arg1, arg2, arg3]),
+      worker(LookupPhoenix.Periodically, [])
     ]
 
     # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
