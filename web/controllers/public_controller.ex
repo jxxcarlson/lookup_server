1765fa17f5ac589a0934f36ef3d56dacded3345e
James Carlson <jxxcarlson@gmail.com>
2017-02-23 10:08:36 -0500

    x

--- web/controllers/public_controller.ex
+++ web/controllers/public_controller.ex
@@ -5,7 +5,28 @@ defmodule LookupPhoenix.PublicController do
     alias LookupPhoenix.Utility
     alias LookupPhoenix.Search
 
-   def show(conn, %{"id" => id}) do
+
+   def share1(conn, %{"id" => id}) do
+         note  = Repo.get(Note, id)
+         token = conn.query_string
+         Utility.report("token", token)
+         if note == nil do
+             render(conn, "error.html", %{})
+         else
+             options = %{mode: "show", process: "none"}
+             params = %{note: note, options: options}
+             case [note.public, note.shared] do
+               [true, _] -> render(conn, "share.html", params, title: "LookupNotes: Public")
+               [_, true] ->
+                  if Note.match_token_array(token, note) do render(conn, "share.html", params, title: "LookupNotes: Shared") end
+               _ ->  render(conn, "error.html", params)
+             end
+         end
+         # match_token_array
+
+     end
+
+   def share(conn, %{"id" => id}) do
         note  = Repo.get(Note, id)
         token = conn.query_string
         Utility.report("token", token)
