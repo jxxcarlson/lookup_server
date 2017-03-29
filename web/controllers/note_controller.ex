2459b3f8de899c88c5ea19e1400a9910c9130db5
James Carlson <jxxcarlson@gmail.com>
2017-03-29 17:16:00 -0400

    Delete dead code

--- web/controllers/note_controller.ex
+++ web/controllers/note_controller.ex
@@ -104,8 +104,6 @@ defmodule LookupPhoenix.NoteController do
   end
 
 
-  ################
-
   def set_channel(conn, params) do
     current_user = conn.assigns.current_user
     Utility.report("PARAMS[SET]", params["set"])
@@ -119,53 +117,6 @@ defmodule LookupPhoenix.NoteController do
       User.update_channel(current_user, channel)
     end
    redirect(conn, to: "/notes?channel=#{channel}")
-  end
-
-  ##########################################################
-
-
-  defp do_show(conn, note) do
-
-   IO.puts("DO SHOW")
-
-    user = Repo.get!(User, note.user_id)
-
-    Note.update_viewed_at(note)
-
-    # Note.add_options(note) -- adds the options
-    #    process: "latex" | "none"
-    #    collate: true | false
-    options = %{mode: "show", username: conn.assigns.current_user.username, public: note.public} |> Note.add_options(note)
-    content = "== " <> note.title <> "\n\n" <> note.content
-    rendered_text = String.trim(RenderText.transform(content, options))
-
-    Utility.report("OPTIONS IN NOTE:SHOW", options)
-
-    inserted_at= Note.inserted_at_short(note)
-    updated_at= Note.updated_at_short(note)
-    word_count = RenderText.word_count(note.content)
-
-    sharing_is_authorized = true #  conn.assigns.current_user.id == note.user_id
-
-    params1 = %{note: note, rendered_text: rendered_text,
-                  inserted_at: inserted_at, updated_at: updated_at,
-                  options: options, word_count: word_count,
-                  sharing_is_authorized: sharing_is_authorized, current_id: note.id, channela: user.channel}
-
-
-    conn_query_string = conn.query_string || ""
-    if conn_query_string == "" do
-      query_string = "index=0&id_string=#{note.id}"
-    else
-      query_string = conn_query_string
-    end
-    params2 = Note.decode_query_string(query_string)
-
-    params = Map.merge(params1, params2)
-
-    # {:ok, updated_at } = note.updated_at |> Timex.local |> Timex.format("{M}-{D}-{YYYY}")
-    # IO.puts "UPDATED AT: #{updated_at}"
-
   end
 
 
